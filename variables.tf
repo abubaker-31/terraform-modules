5184da0c4da87bb7e271050be1cb44b0354c2d04
Abubaker <80399346+abubaker-31@users.noreply.github.com>
2024-06-20 15:22:51 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -44,3 +44,112 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
+variable "alb_name" {
+  description = "The name of the ALB"
+  type        = string
+}
+
+variable "internal" {
+  description = "Boolean to determine if the ALB is internal or external"
+  type        = bool
+}
+
+variable "security_groups" {
+  description = "The security groups associated with the ALB"
+  type        = list(string)
+}
+
+variable "subnets" {
+  description = "The subnets associated with the ALB"
+  type        = list(string)
+}
+
+variable "enable_deletion_protection" {
+  description = "Boolean to enable deletion protection on the ALB"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "target_group_name" {
+  description = "The name of the target group"
+  type        = string
+}
+
+variable "target_group_port" {
+  description = "The port for the target group"
+  type        = number
+}
+
+variable "target_group_protocol" {
+  description = "The protocol for the target group"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The VPC ID where the target group is located"
+  type        = string
+}
+
+variable "health_check_interval" {
+  description = "The interval for health checks"
+  type        = number
+  default     = 30
+}
+
+variable "health_check_path" {
+  description = "The path for health checks"
+  type        = string
+  default     = "/"
+}
+
+variable "health_check_port" {
+  description = "The port for health checks"
+  type        = string
+  default     = "traffic-port"
+}
+
+variable "health_check_protocol" {
+  description = "The protocol for health checks"
+  type        = string
+  default     = "HTTP"
+}
+
+variable "health_check_timeout" {
+  description = "The timeout for health checks"
+  type        = number
+  default     = 5
+}
+
+variable "healthy_threshold" {
+  description = "The number of successful health checks required before considering an instance healthy"
+  type        = number
+  default     = 3
+}
+
+variable "unhealthy_threshold" {
+  description = "The number of failed health checks required before considering an instance unhealthy"
+  type        = number
+  default     = 3
+}
+
+variable "listener_port" {
+  description = "The port for the listener"
+  type        = number
+  default     = 80
+}
+
+variable "listener_protocol" {
+  description = "The protocol for the listener"
+  type        = string
+  default     = "HTTP"
+}
+
+
+
+
